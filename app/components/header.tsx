4ab35885b6e4d68ede14e773c2e112ec9db52f96
Pierrick <moreaupierrick.mp@gmail.com>
2023-06-06 22:31:20 +0200

    Tentative de navbar en cours

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,38 +4,35 @@ import React from "react";
 import iconeResRel from "../../public/assets/icon.png";
 import { IonIcon } from "@ionic/react";
 import { notifications } from "ionicons/icons";
-import { Button, Avatar, Row, Card, Spacer, Tooltip } from "@nextui-org/react";
+import { Button, Avatar, Row, Card, Spacer, Tooltip, Navbar, styled, Image } from "@nextui-org/react";
 import AvatarTooltip from "./AvatarTooltip";
+import Layout from "../layout";
 
 const espacement = 1;
+const Box = styled("div", {
+  boxSizing: "border-box",
+});
 
 const UtilisateurConnecteLinks = () => {
   return (
     <>
-      <Link href="/pages/ressources">
-        <b>Ressources</b>
-      </Link>
-      <Spacer x={espacement} />
-
-      <Link href="/pages/activites">
-        <b>Activités</b>
-      </Link>
-      <Spacer x={espacement} />
-
-      <Link href="/pages/creationRessource">
-        <b>Créer une ressource</b>
-      </Link>
-      <Spacer x={espacement} />
-
-      <Link href="/pages/relations">
-        <b>Mes relations</b>
-      </Link>
-      <div style={{ flex: 1 }}></div>
-
-      <Link href="/pages/notifications">
+      <Navbar.Content>
+        <Navbar.Link color="primary" href="/pages/ressources">
+          <b>Ressources</b>
+        </Navbar.Link>
+        <Navbar.Link color="primary" href="/pages/activites">
+          <b>Activités</b>
+        </Navbar.Link>
+        <Navbar.Link color="primary" href="/pages/creationRessource">
+          <b>Créer une ressource</b>
+        </Navbar.Link>
+        <Navbar.Link color="primary" href="/pages/relations">
+          <b>Mes relations</b>
+        </Navbar.Link>
+      </Navbar.Content>
+      <Navbar.Link color="primary" href="/pages/notifications">
         <IonIcon icon={notifications} />
-      </Link>
-      <Spacer x={espacement} />
+      </Navbar.Link>
 
       {/* <img
         src="https://media.tenor.com/vxFNoJHV3I4AAAAC/chiquichico.gif"
@@ -43,15 +40,16 @@ const UtilisateurConnecteLinks = () => {
         className="image-profil"
         height="60px"
       /> */}
-      <Tooltip content={<AvatarTooltip />} placement="bottom">
+      <Tooltip content={<AvatarTooltip />} placement="bottom" >
         <Avatar
           src="https://media.tenor.com/vxFNoJHV3I4AAAAC/chiquichico.gif"
-          alt="Logo res rel"
+          alt="photo profil"
         />
       </Tooltip>
-      <Spacer x={espacement} />
 
-      <Button>Se déconnecter</Button>
+      <Navbar.Item>
+        <Button auto flat>Se déconnecter</Button>
+      </Navbar.Item>
     </>
   );
 };
@@ -67,23 +65,31 @@ const InviteLinks = () => {
 
 const Header = () => {
   return (
-    <Card
-    //  css={{ $$cardColor: "$colors$primary" }}
-    >
-      <Card.Body>
-        <Row justify="center" align="center">
-          <Spacer x={espacement} />
+    <>
+      <Box css={{ maxW: "100%" }}>
+        <Navbar isBordered variant="sticky">
+          <Navbar.Toggle showIn="xs" />
+          <Navbar.Brand>
+            <Link href="/">
+              <Image
+                width={50}
+                height={50}
+                src={iconeResRel.src}
+                alt="Logo res rel" />
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Content
+            enableCursorHighlight
+            activeColor="secondary"
+            hideIn="xs"
+            variant="highlight-rounded"
+          >
+            <UtilisateurConnecteLinks />
+          </Navbar.Content>
+        </Navbar>
+      </Box>
+    </>
 
-          <Link href="/">
-            <Avatar src={iconeResRel.src} alt="Logo res rel" />
-          </Link>
-
-          <Spacer x={espacement} />
-
-          <UtilisateurConnecteLinks />
-        </Row>
-      </Card.Body>
-    </Card>
   );
 };
 
