8886b86335e92bfc2deb07a55d402e4cdef4f5f7
Pierrick <moreaupierrick.mp@gmail.com>
2023-06-06 15:35:15 +0200

    Style page demande relation

--- app/components/DemandeRelation.tsx
+++ app/components/DemandeRelation.tsx
@@ -1,34 +1,39 @@
 "use client";
 import React from "react";
 import { LiensTelechargementsEnum } from "../ressources/enums/LiensTelechargements";
+import { Button, Card, Row } from "@nextui-org/react";
 
 const DemandeRelation = ({ demandeRelation }: any) => {
+
   return (
-    <div>
-      <div className="demande-relation">
-        <img
-          src={
-            LiensTelechargementsEnum.PHOTOPROFILURL +
+
+    <Card isPressable isHoverable>
+
+      <Card.Body css={{ p: 0 }} >
+        <Card.Image
+          objectFit="cover"
+          width="100%"
+          height={450}
+          src={LiensTelechargementsEnum.PHOTOPROFILURL +
             demandeRelation.idDemandeur +
-            LiensTelechargementsEnum.DOWNLOAD
-          }
-          alt="avatar"
+            LiensTelechargementsEnum.DOWNLOAD}
           className="image-profil"
         />
-        <p>
+      </Card.Body>
+      <Card.Footer css={{ justifyItems: "flex-start" }}>
+        <Row gap={0} wrap="wrap" justify="space-between" align="center" >
           <b>
             {demandeRelation.demandeur.nom} {demandeRelation.demandeur.prenom}{" "}
           </b>
           demande une relation de type :
           <b> {demandeRelation.typeRelation.nom}</b>
-        </p>
-        <div className="buttons">
-          <button className="btn-see-ressource">Voir profil</button>
-          <button className="btn-add-relation">Accepter relation</button>
-          <button className="btn-add-relation">Refuser relation</button>
-        </div>
-      </div>
-    </div>
+
+          <Button size="sm" color="success" auto ghost>Accepter relation</Button>
+          <Button size="sm" color="error" auto ghost>Refuser relation</Button>
+        </Row>
+      </Card.Footer>
+    </Card>
+
   );
 };
 
